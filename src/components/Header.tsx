6eb607744975bdc0ac4d86cc4f0e07fa21a43672
digitrove <digitrove.agency@gmail.com>
2025-07-15 22:28:13 +0500

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,7 +19,7 @@ const Header: React.FC = () => {
       transition={{ duration: 0.6 }}
       className="fixed top-0 left-0 right-0 z-50 bg-black/90 backdrop-blur-md border-b border-gray-800"
     >
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto px-3 sm:px-6 lg:px-6">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <Monitor className="w-8 h-8 text-yellow-400 mr-3" />

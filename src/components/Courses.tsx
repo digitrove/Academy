9a58fd18dfd3447af926f211cca666e7f79626d7
digitrove <digitrove.agency@gmail.com>
2025-07-15 23:01:22 +0500

    Updated Courses.tsx

--- src/components/Courses.tsx
+++ src/components/Courses.tsx
@@ -188,7 +188,7 @@ const Courses: React.FC = () => {
                   type: "spring",
                   stiffness: 200
                 }}
-                className="bg-gradient-to-r from-yellow-400 to-yellow-600 text-black text-xs font-bold px-3 py-1.5 rounded-full shadow-lg border-2 border-white"
+                className=""
               >
                 <span className="">100% FREE</span>
               </motion.div>
